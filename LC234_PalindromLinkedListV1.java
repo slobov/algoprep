a1daef2a13444dc0dc46a311532525b1a3fa4a0b
Sergey <lobov_serega@mail.ru>
2018-07-30 22:30:15 -0700

    Create LC234_PalindromLinkedListV1.java

---
+++ LC234_PalindromLinkedListV1.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        
+        for (int i = 0, j = list.size() - 1; i < j; i++, j--) {
+            if (!list.get(i).equals(list.get(j))) {
+                return false;
+            }
+        }
+        
+        return true;  
+        
+    }
+}
